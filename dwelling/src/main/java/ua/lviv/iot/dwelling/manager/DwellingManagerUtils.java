6be7f42ae0d07a4a6ed9da1923041c635ef50fea
Bogdan Bedrylo <bogdan.bedrylo@gmail.com>
2020-03-16 18:48:44 +0200

    4 lab

---
+++ dwelling/src/main/java/ua/lviv/iot/dwelling/manager/DwellingManagerUtils.java
@@ -0,0 +1,66 @@
+package ua.lviv.iot.dwelling.manager;
+
+import java.util.Comparator;
+import java.util.List;
+import ua.lviv.iot.dwelling.model.AbstractDwelling;
+import ua.lviv.iot.dwelling.model.SortType;
+
+public class DwellingManagerUtils {
+
+	private static final DwellingSorterByPrice DWELLING_BY_PRICE_SORTER = new DwellingSorterByPrice();
+
+	public static void sortByArea(List<AbstractDwelling> dwellings, SortType sortType) {
+		DwellingManagerUtils utils = new DwellingManagerUtils();
+		dwellings.sort(sortType == SortType.ASC ? utils.new DwellingSorterByArea()
+				: utils.new DwellingSorterByArea().reversed());
+	}
+
+	public static void sortByPrice(List<AbstractDwelling> dwellings, SortType sortType) {
+		dwellings.sort(sortType == SortType.ASC ? DWELLING_BY_PRICE_SORTER : DWELLING_BY_PRICE_SORTER.reversed());
+	}
+
+	public static void sortByPriceAndArea(List<AbstractDwelling> dwellings, SortType sortType) {
+		Comparator<AbstractDwelling> comparator = new Comparator<AbstractDwelling>() {
+			@Override
+			public int compare(AbstractDwelling firstDwelling, AbstractDwelling secondDwelling) {
+				double priceComparisonResult = firstDwelling.getPriceInUSD() - secondDwelling.getPriceInUSD();
+				if (priceComparisonResult != 0) {
+					return (int) priceComparisonResult;
+				} else {
+					return (int) (firstDwelling.getAreaInSquareMeters() - secondDwelling.getAreaInSquareMeters());
+				}
+			}
+		};
+		dwellings.sort(sortType == SortType.ASC ? comparator : comparator.reversed());
+	}
+
+	public static void sortByDistrict(List<AbstractDwelling> dwellings, SortType sortType) {
+		switch (sortType) {
+		case ASC:
+			dwellings.sort((AbstractDwelling firstDwelling,
+					AbstractDwelling secondDwelling) -> firstDwelling.getDistrict().compareTo(secondDwelling.getDistrict()));
+			break;
+		case DESC:
+			dwellings.sort((AbstractDwelling firstDwelling,
+					AbstractDwelling secondDwelling) -> secondDwelling.getDistrict().compareTo(firstDwelling.getDistrict()));
+			break;
+		default:
+			break;
+		}
+	}
+
+	class DwellingSorterByArea implements Comparator<AbstractDwelling> {
+		@Override
+		public int compare(AbstractDwelling firstDwelling, AbstractDwelling secondDwelling) {
+			return (int) (firstDwelling.getAreaInSquareMeters() - secondDwelling.getAreaInSquareMeters());
+		}
+	}
+
+	static class DwellingSorterByPrice implements Comparator<AbstractDwelling> {
+		@Override
+		public int compare(AbstractDwelling firstDwelling, AbstractDwelling secondDwelling) {
+			return (int) (firstDwelling.getPriceInUSD() - secondDwelling.getPriceInUSD());
+		}
+	}
+
+}
